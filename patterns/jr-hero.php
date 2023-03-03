33e4d7bef11be1f1af0340df5faec518bb5b691d
Alfredo Navas <elpuas@gmail.com>
2023-03-02 22:00:30 -0600

    pahse one final

--- patterns/jr-hero.php
+++ patterns/jr-hero.php
@@ -13,7 +13,7 @@
 ?>
 
 <!-- wp:cover {"url":"http://jr.local/wp-content/uploads/2023/03/cover-image-black-jpg.webp","id":83,"dimRatio":0,"overlayColor":"gray","focalPoint":{"x":0.11,"y":0.45},"minHeight":661,"minHeightUnit":"px","templateLock":"all","lock":{"move":true,"remove":true}} -->
-<div class="wp-block-cover" style="min-height:661px"><span aria-hidden="true" class="wp-block-cover__background has-gray-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-83" alt="" src="http://jr.local/wp-content/uploads/2023/03/cover-image-black-jpg.webp" style="object-position:11% 45%" data-object-fit="cover" data-object-position="11% 45%"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-cover" style="min-height:661px"><span aria-hidden="true" class="wp-block-cover__background has-gray-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-83" alt="" src="https://jr.cr/wp-content/uploads/2023/03/cover-image-black-jpg.webp" style="object-position:11% 45%" data-object-fit="cover" data-object-position="11% 45%"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"layout":{"type":"constrained"}} -->
 <div class="wp-block-column"></div>
 <!-- /wp:column -->
@@ -21,7 +21,11 @@
 <!-- wp:column {"layout":{"type":"constrained"}} -->
 <div class="wp-block-column"><!-- wp:heading {"level":1,"style":{"typography":{"textTransform":"uppercase"}},"textColor":"primary","fontFamily":"eveleth-clean"} -->
 <h1 class="wp-block-heading has-primary-color has-text-color has-eveleth-clean-font-family" style="text-transform:uppercase">Vision Humanista de un Futuro Innovador</h1>
-<!-- /wp:heading --></div>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"size-max-36","fontFamily":"montserrat-alternates"} -->
+<p class="has-montserrat-alternates-font-family has-size-max-36-font-size"><em>Jose Rafael Quesada</em></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div></div>
 <!-- /wp:cover -->
